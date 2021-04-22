c40b5905bb2856bd124955ed9b425f6f483c0026
mstovall80 <michael_stovall2001@yahoo.com>
2021-04-22 18:54:27 -0500

    first commit

---
+++ src/main/java/com/stovall/stringAssignment/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.stovall.stringAssignment;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StringAssignmentApplication.class);
+	}
+
+}
---
+++ src/main/java/com/stovall/stringAssignment/StringAssignmentApplication.java
@@ -0,0 +1,26 @@
+package com.stovall.stringAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// 2. Importing dependencies
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+// 1. Annotation
+@RestController
+public class StringAssignmentApplication {
+        public static void main(String[] args) {
+                SpringApplication.run(StringAssignmentApplication.class, args);
+        }
+        
+        // 1. Annotation
+        @RequestMapping("/")
+        // 3. Method that maps to the request route above
+        public String hello() { // 3
+                return "Hello Client! How are you doing?";
+        }
+        @RequestMapping("/random")
+        public String responce() {
+        	return "Spring Boot is great! So easy to just respond with strings.";
+        }
+}
